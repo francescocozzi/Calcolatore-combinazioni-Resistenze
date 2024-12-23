a00dcff8a565a7cf698bafccd8ea2db9406c3777
francescocozzi <francesco@iz7khr.it>
2024-12-23 12:46:17 +0100

    Fix build configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  distDir: '.next',
   reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
